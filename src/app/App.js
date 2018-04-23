30f6a22cc4908662dc72521aa1730269264e8a33
Boris Shevchenko <bshevchenkoweb@gmail.com>
2018-04-23 22:15:20 +0700

    #29 refactored sign-up logic extracted into the polymath-ui repo

--- src/app/App.js
+++ src/app/App.js
@@ -5,12 +5,10 @@ import React, { Component } from 'react'
 import Contract from 'polymathjs'
 import { renderRoutes } from 'react-router-config'
 import { connect } from 'react-redux'
-import { PolymathUI, txHash, txEnd } from 'polymath-ui'
+import { PolymathUI, SignUpPage, initAccount, txHash, txEnd } from 'polymath-ui'
 import type { RouterHistory } from 'react-router-dom'
 
 import Root from './Root'
-import SignUpPage from './account/SignUpPage'
-import { init as initAccount } from './account/actions'
 import type { RootState } from '../redux/reducer'
 
 type StateProps = {|
@@ -28,8 +26,8 @@ type DispatchProps = {|
 
 const mapStateToProps = (state: RootState): StateProps => ({
   network: state.network,
-  isSignedUp: state.account.isSignedUp,
-  balance: state.account.balance,
+  isSignedUp: state.pui.account.isSignedUp,
+  balance: state.pui.account.balance,
   ticker: state.token.token ? state.token.token.ticker : null,
 })
 
@@ -59,10 +57,10 @@ class App extends Component<Props> {
   }
 
   render () {
-    const { history, balance, ticker, isSignedUp } = this.props
+    const { history, ticker, isSignedUp } = this.props
     return (
       <Root>
-        <PolymathUI history={history} balance={balance} ticker={ticker} />
+        <PolymathUI history={history} ticker={ticker} />
         {isSignedUp ? renderRoutes(this.props.route.routes) : <SignUpPage />}
       </Root>
     )
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,21 +4,19 @@ import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
 import { reducer as network } from 'polymath-auth'
 import { reducer as pui } from 'polymath-ui'
+// import type { PUIState } from 'polymath-ui'
 import type { NetworkState } from 'polymath-auth/types'
 
-import account from '../app/account/reducer'
 import token from '../app/token/reducer'
 import sto from '../app/sto/reducer'
 import whitelist from '../app/dashboard/whitelist/reducer'
 import type { WhitelistState } from '../app/dashboard/whitelist/reducer'
-import type { AccountState } from '../app/account/reducer'
 import type { TokenState } from '../app/token/reducer'
 import type { STOState } from '../app/sto/reducer'
 
 export default combineReducers({
   network,
   form,
-  account,
   token,
   sto,
   pui,
@@ -28,11 +26,10 @@ export default combineReducers({
 export type RootState = {
   network: NetworkState,
   form: any,
-  account: AccountState,
   token: TokenState,
   sto: STOState,
   whitelist: WhitelistState,
-  pui: any,
+  pui: any, // TODO @bshevchenko: https://github.com/PolymathNetwork/polymath-issuer/issues/14
 }
 
 export type GetState = () => RootState
