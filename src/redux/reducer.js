f6fa5d9e34b5e97d9f194a107bd249db2ecb8cfb
Boris Shevchenko <bshevchenkoweb@gmail.com>
2018-04-27 12:58:29 +0700

    metamask guidance via polymath-auth@1.0.0 & polymath-ui@1.2.0

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,7 +5,7 @@ import { reducer as form } from 'redux-form'
 import { reducer as network } from 'polymath-auth'
 import { reducer as pui } from 'polymath-ui'
 // import type { PUIState } from 'polymath-ui'
-import type { NetworkState } from 'polymath-auth/types'
+import type { NetworkState } from 'polymath-auth'
 
 import token from '../app/token/reducer'
 import sto from '../app/sto/reducer'
