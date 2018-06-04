c19c69a42ece2e7117b21457086e5b529f700736
Shannon Clarke <shannonjamalclarke@gmail.com>
2018-06-04 18:44:20 -0400

    Add dummy page

--- src/RouteLoader.js
+++ src/RouteLoader.js
@@ -9,18 +9,49 @@ import { MetamaskPage } from 'polymath-ui'
 
 import Root from './app/Root'
 import SplashPage from './app/SplashPage'
+import DummyPage from './app/DummyPage'
 import routes from './routes'
 
 type Props = {
   location: {
     pathname: string,
-    [any]: any
-  }
+    [any]: any,
+  },
 }
 
-class RouteLoader extends Component<Props> {
+type State = {|
+  screen_width: number,
+|}
+
+class RouteLoader extends Component<Props, State> {
+  constructor () {
+    super()
+    this.state = {
+      screen_width: window.innerWidth,
+    }
+  }
+  componentWillMount () {
+    window.addEventListener('resize', this.handleWindowSizeChange)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.handleWindowSizeChange)
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ screen_width: window.innerWidth })
+  }
+
   render () {
-    if (this.props.location.pathname === '/') {
+    const { screen_width } = this.state
+    const isMobile = screen_width <= 768
+    if (isMobile || typeof window.orientation !== 'undefined') {
+      return (
+        <Root>
+          <DummyPage />
+        </Root>
+      )
+    } else if (this.props.location.pathname === '/') {
       // noinspection RequiredAttributes
       return (
         <Root>
@@ -28,9 +59,7 @@ class RouteLoader extends Component<Props> {
         </Root>
       )
     }
-    const networks = [
-      NETWORK_RINKEBY,
-    ]
+    const networks = [ NETWORK_RINKEBY ]
     return (
       <PolymathAuth loading={<Loading />} guide={<MetamaskPage networks='Rinkeby' />} networks={networks}>
         {renderRoutes(routes)}
