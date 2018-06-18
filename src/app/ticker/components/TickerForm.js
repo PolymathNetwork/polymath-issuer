39c7194ebab5ab0ff413bb585dfa05c9331897a8
Shannon Clarke <shannonjamalclarke@gmail.com>
2018-06-18 11:42:10 -0400

    Fix UI: Replace the 4 character limit for the token symbol with a 10 char limit for the token symbol

--- src/app/ticker/components/TickerForm.js
+++ src/app/ticker/components/TickerForm.js
@@ -84,7 +84,7 @@ export default reduxForm({
   asyncValidate: async (values) => {
     // async validation doesn't work properly with field-level validation, so we need to specify sync rules here
     const v = values.ticker
-    const syncError = required(v) || maxLength(4)(v) || alphanumeric(v)
+    const syncError = required(v) || maxLength(10)(v) || alphanumeric(v)
     if (syncError) {
       // eslint-disable-next-line
       throw { ticker: syncError }
