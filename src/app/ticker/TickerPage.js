37d7f66d1392d026ffd48a90dc0a2a4bd644c898
Jose Herrera <jose@polymath.network>
2018-06-28 10:55:50 -0400

    starting to implement https://app.asana.com/0/724140591411017/700480059286882

--- src/app/ticker/TickerPage.js
+++ src/app/ticker/TickerPage.js
@@ -6,7 +6,7 @@ import DocumentTitle from 'react-document-title'
 import { change } from 'redux-form'
 import { bull } from 'polymath-ui'
 import { Redirect } from 'react-router'
-import { TickerRegistry } from 'polymathjs'
+import { TickerRegistry, types } from 'polymathjs'
 import type { RouterHistory } from 'react-router'
 import {
   Button,
@@ -27,19 +27,23 @@ type StateProps = {|
   token: Object,
   isRegistered: boolean,
   transaction: TickerTransaction,
+  networkName: string,
+  polyBalance: types._bignumber
 |}
 
 type DispatchProps = {|
-  change: (?string) => any,
-  register: () => any,
-  tokenData: (data: any) => any
-|}
+  change: (? string) => any,
+    register: () => any,
+      tokenData: (data: any) => any
+        |}
 
 const mapStateToProps = (state): StateProps => ({
   account: state.network.account,
   token: state.token.token,
   isRegistered: state.ticker.isRegistered,
   transaction: state.ticker.transaction,
+  networkName: state.network.name,
+  polyBalance: state.pui.account.balance,
 })
 
 const mapDispatchToProps: DispatchProps = {
@@ -50,17 +54,19 @@ const mapDispatchToProps: DispatchProps = {
 
 type Props = {|
   history: RouterHistory
-|} & StateProps & DispatchProps
+    |} & StateProps & DispatchProps
 
 type State = {|
-  isModalOpen: boolean,
-  expiryLimit: number,
+  isConfirmationModalOpen: boolean,
+    isNotEnoughPolyModalOpen: boolean,
+      expiryLimit: number,
 |}
 
 class TickerPage extends Component<Props, State> {
 
   state = {
-    isModalOpen: false,
+    isConfirmationModalOpen: false,
+    isNotEnoughPolyModalOpen: false,
     expiryLimit: 7,
   }
 
@@ -74,16 +80,29 @@ class TickerPage extends Component<Props, State> {
   }
 
   handleSubmit = () => {
-    this.setState({ isModalOpen: true })
+    this.setState({ isConfirmationModalOpen: true })
   }
 
   handleConfirm = () => {
-    this.setState({ isModalOpen: false })
-    this.props.register()
+    this.setState({ isConfirmationModalOpen: false })
+    if (this.props.polyBalance < 250) {
+      this.setState({ isNotEnoughPolyModalOpen: true })
+    } else {
+      this.props.register()
+    }
   }
 
-  handleCancel = () => {
-    this.setState({ isModalOpen: false })
+  handleConfirmationCancel = () => {
+    this.setState({ isConfirmationModalOpen: false })
+  }
+
+  handleNotEnoughPolyCancel = () => {
+    this.setState({ isNotEnoughPolyModalOpen: false })
+  }
+
+  handleFaucetRequest = () => {
+    // console.log('requesting 25k POLY from contract: 0x455Da7D06862Fa7d7639473F287f88bc7b35FF7F')
+    // console.log('to address:' + this.props.account)
   }
 
   render () {
@@ -94,7 +113,7 @@ class TickerPage extends Component<Props, State> {
     return (
       <DocumentTitle title='Token Symbol Reservation – Polymath'>
         <Fragment>
-          <ComposedModal open={this.state.isModalOpen} className='pui-confirm-modal'>
+          <ComposedModal open={this.state.isConfirmationModalOpen} className='pui-confirm-modal'>
             <ModalHeader
               label='Confirmation required'
               title={(
@@ -121,12 +140,85 @@ class TickerPage extends Component<Props, State> {
             </ModalBody>
 
             <ModalFooter>
-              <Button kind='secondary' onClick={this.handleCancel}>
+              <Button kind='secondary' onClick={this.handleConfirmationCancel}>
                 Cancel
               </Button>
               <Button onClick={this.handleConfirm}>Reserve Ticker</Button>
             </ModalFooter>
           </ComposedModal>
+
+          <ComposedModal
+            open={this.state.isNotEnoughPolyModalOpen && this.props.networkName === 'Kovan Testnet'}
+            className='pui-confirm-modal'
+          >
+            <ModalHeader
+              label='Confirmation required'
+              title={(
+                <span>
+                  <Icon name='warning--glyph' fill='#E71D32' width='24' height='24' />&nbsp;
+                    Transaction Impossible
+                </span>
+              )}
+            />
+
+            <ModalBody>
+              <div className='bx--modal-content__text'>
+                <p>
+                  The registration of a token symbol has a fixed cost of 250 POLY. 
+                  Please make sure that your wallet has a sufficient balance in
+                  POLY to complete this operation.
+                </p>
+
+                <p>
+                  You are currently connected to the Kovan Test Network.
+                </p>
+                <p>
+                  As such, you can click on the &laquo;REQUEST 25K POLY&raquo; button below to
+                   receive 25,000 test POLY in your wallet.
+                </p>
+
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button kind='secondary' onClick={this.handleNotEnoughPolyCancel}>
+                Cancel
+              </Button>
+              <Button onClick={this.handleFaucetRequest}>REQUEST 25k POLY</Button>
+            </ModalFooter>
+          </ComposedModal>
+          <ComposedModal
+            open={this.state.isNotEnoughPolyModalOpen && this.props.networkName === 'Ethereum Mainnet'} 
+            className='pui-confirm-modal'
+          >
+            <ModalHeader
+              label='Confirmation required'
+              title={(
+                <span>
+                  <Icon name='warning--glyph' fill='#E71D32' width='24' height='24' />&nbsp;
+                  Transaction Impossible
+                </span>
+              )}
+            />
+
+            <ModalBody>
+              <div className='bx--modal-content__text'>
+                <p>
+                  The registration of a token symbol has a fixed cost of 250 POLY. 
+                  Please make sure that your wallet has a sufficient balance in
+                  POLY to complete this operation.
+                </p>
+                <p>
+                  You can purchase POLY at your favourite exchange.
+                </p>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button kind='secondary' onClick={this.handleNotEnoughPolyCancel}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </ComposedModal>
+
           <div className='pui-single-box'>
             <div className='pui-single-box-header'>
               <div className='pui-single-box-bull'>
