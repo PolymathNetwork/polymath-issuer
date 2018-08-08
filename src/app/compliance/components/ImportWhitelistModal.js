7a2b06eb92ad20a78e76c5b5b0a83a81d91fc092
Boris Shevchenko <bshevchenkoweb@gmail.com>
2018-08-08 14:33:44 +0300

    import csv debug console logs 2

--- src/app/compliance/components/ImportWhitelistModal.js
+++ src/app/compliance/components/ImportWhitelistModal.js
@@ -67,6 +67,7 @@ class ImportWhitelistModal extends Component<Props> {
     console.log('handleUploaded event.target.files[0]', event.target.files[0]) // eslint-disable-next-line no-console
     // eslint-disable-next-line
     const file = event.target.files[0] // eslint-disable-next-line no-console
+    console.log('handleUploaded file.type', file.type) // eslint-disable-next-line no-console
     console.log('handleUploaded file.type.match', file.type.match(/csv.*/))
     if (file.type.match(/csv.*/)) { // eslint-disable-next-line no-console
       console.log('handleUploaded matched file', file)
